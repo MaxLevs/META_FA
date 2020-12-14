a1b9bf82cb76f1625e4bb5f0d2b43b76383b03cd
Maxim Liven <maxlevs@mail.ru>
2020-12-15 01:33:36 +0300

    Add MachineDetermined.EquivalentK() method

--- META_FA/StateMachine/MachineDetermined.cs
+++ META_FA/StateMachine/MachineDetermined.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using META_FA.StateMachine.Exceptions;
 
 namespace META_FA.StateMachine
@@ -15,7 +17,31 @@ namespace META_FA.StateMachine
                 throw new DuplicateTransitionException(newTransition, this);
             }
         }
+        
+        private bool EquivalentK(State stateOne, State stateTwo, int k)
+        {
+            if (k < 0) throw new ArgumentException("Argument k can't be less than 0", nameof(k));
 
+            if (k == 0)
+            {
+                return stateOne.IsFinal == stateTwo.IsFinal;
+            }
+
+            var nextStatesOne = _transitions.Where(transition => Equals(transition.StartState, stateOne)).Select(transition => transition.EndState).ToList();
+            var nextStatesTwo = _transitions.Where(transition => Equals(transition.StartState, stateTwo)).Select(transition => transition.EndState).ToList();
+
+            if (!EquivalentK(stateOne, stateTwo, k - 1))
+            {
+                return false;
+            }
+
+            return !nextStatesOne
+                .SelectMany(nextStateOne => nextStatesTwo,
+                    (nextStateOne, nextStateTwo) => new {nextStateOne, nextStateTwo})
+                .Where(@t => !EquivalentK(@t.nextStateOne, @t.nextStateTwo, k - 1))
+                .Select(@t => @t.nextStateOne).Any();
+        }
+        
         public override Machine Minimize()
         {
             throw new System.NotImplementedException();
