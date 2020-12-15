69bbf6c5d614393e81848ca0da6b48a60db7244d
Maxim Liven <maxlevs@mail.ru>
2020-12-15 21:59:12 +0300

    Epsilon transitions

--- META_FA/Options/TransitionOptions.cs
+++ META_FA/Options/TransitionOptions.cs
@@ -5,5 +5,6 @@ namespace META_FA.Options
         public string StartState { get; set; }
         public string Token { get; set; }
         public string EndState { get; set; }
+        public bool IsEpsilon { get; set; }
     }
 }
