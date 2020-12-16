5eab18f4b52426b8049f40453f3cc1f85e42e2fd
Maxim Liven <maxlevs@mail.ru>
2020-12-17 00:27:54 +0300

    New table generator

--- META_FA/Options/SMOptions.cs
+++ META_FA/Options/SMOptions.cs
@@ -27,25 +27,62 @@ namespace META_FA.Options
 
         public string ToTable()
         {
+            var tokens = Transitions.Select(tr => tr.Token).Distinct().ToList();
+            
             var states = GetStates().ToList();
-            var padding = Math.Max(states.Max(s => s.Length), Transitions.Max(tr => tr.Token?.Length ?? 1)) + 2;
-            if (Transitions.Any(tr => tr.IsEpsilon))
+
+            var table = Transitions.GroupBy(tr => new {tr.StartState, tr.Token}).ToList();
+
+            var pad = table
+                .GroupBy(tableCell => tableCell.Key.Token)
+                .Select(x => Math.Max(x.Key?.Length ?? 3, x.Max(cell => Math.Max(cell.Key.StartState.Length, string.Join(",", cell.Select(tr => tr.EndState)).Length))) + 4)
+                .ToList();
+            var paddings = new List<int> { " State ".Length };
+            paddings.AddRange(pad);
+            var padding = paddings.Max();
+            
+            string result = "State".PadLeft(padding) + string.Join("", tokens.Select(token => $"  {token ?? "[ε]"}  ".PadLeft(padding))) + "\n";
+            foreach (var state in states)
             {
-                padding = Math.Max(padding, 3 + 2);
-            }
-            var headline = states.Aggregate(">".PadLeft(padding), (result, stateCol) => result + stateCol.PadLeft(padding));
-            return states.Aggregate(headline, (resultRow, stateRow) => resultRow + "\n" + states.Aggregate(stateRow.PadLeft(padding),
-                (resultCol, stateCol) =>
+                result += state.PadLeft(padding);
+                
+                foreach (var token in tokens)
                 {
-                    var tr = Transitions.Find(tr => tr.StartState == stateCol && tr.EndState == stateRow);
-                    
-                    if (tr == null)
+                    var cell = table.Find(x => x.Key.StartState == state && x.Key.Token == token);
+
+                    if (cell == null)
                     {
-                        return resultCol + ".".PadLeft(padding);
+                        result += new string(' ', padding);
+                        continue;
                     }
-                    
-                    return resultCol + (tr.Token ?? (tr.IsEpsilon ? "[ε]" : ".")).PadLeft(padding);
-                }));
+
+                    result += $" {string.Join(",", cell.Select(tr => tr.EndState))}  ".PadLeft(padding);
+                }
+
+                result += "\n";
+            }
+
+            return result;
+
+            // var states = GetStates().ToList();
+            // var padding = Math.Max(states.Max(s => s.Length), Transitions.Max(tr => tr.Token?.Length ?? 1)) + 2;
+            // if (Transitions.Any(tr => tr.IsEpsilon))
+            // {
+            //     padding = Math.Max(padding, 3 + 2);
+            // }
+            // var headline = states.Aggregate(">".PadLeft(padding), (result, stateCol) => result + stateCol.PadLeft(padding));
+            // return states.Aggregate(headline, (resultRow, stateRow) => resultRow + "\n" + states.Aggregate(stateRow.PadLeft(padding),
+            //     (resultCol, stateCol) =>
+            //     {
+            //         var tr = Transitions.Find(tr => tr.StartState == stateCol && tr.EndState == stateRow);
+            //         
+            //         if (tr == null)
+            //         {
+            //             return resultCol + ".".PadLeft(padding);
+            //         }
+            //         
+            //         return resultCol + (tr.Token ?? (tr.IsEpsilon ? "[ε]" : ".")).PadLeft(padding);
+            //     }));
         }
 
         public string ToDot()
@@ -73,7 +110,7 @@ namespace META_FA.Options
                 node.Attributes.FillColor = Color.Gray;
             });
 
-            foreach (var @group in Transitions.GroupBy(options => new {options.StartState, options.EndState}))
+            foreach (var group in Transitions.GroupBy(options => new {options.StartState, options.EndState}))
             {
                 graph.Edges.Add(group.Key.StartState, group.Key.EndState, edge =>
                 {
