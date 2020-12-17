c9b899966cd2ab938742a4d84bf69a0f8c69393f
Maxim Liven <maxlevs@mail.ru>
2020-12-17 13:24:56 +0300

    RegexCST.cs: Equals() and GetHashCode()

--- META_FA/CST/RegexCST.cs
+++ META_FA/CST/RegexCST.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System;
 using META_FA.Visitors.CST;
 
@@ -7,5 +8,15 @@ namespace META_FA.CST
     {
         public abstract Guid Id { get; }
         public abstract void Visit(CSTVisitor visitor);
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RegexCST regexCst && Equals(Id, regexCst.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
