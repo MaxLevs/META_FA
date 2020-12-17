0320f651531b0736083f1ba7503feaa345d054d4
Maxim Liven <maxlevs@mail.ru>
2020-12-17 19:23:44 +0300

    Worked CSTDotVisitor.cs

--- META_FA/Visitors/CST/CSTDotVisitor.cs
+++ META_FA/Visitors/CST/CSTDotVisitor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using GiGraph.Dot.Entities.Edges;
 using GiGraph.Dot.Entities.Graphs;
 using GiGraph.Dot.Entities.Nodes;
@@ -9,6 +10,7 @@ namespace META_FA.Visitors.CST
     public class CSTDotVisitor : CSTVisitor
     {
         private readonly DotGraph _graph = new DotGraph(directed: true);
+        private readonly List<DotNode> _nodes = new List<DotNode>();
         
         public string Result {
             get
@@ -28,6 +30,7 @@ namespace META_FA.Visitors.CST
                     var node = new DotNode(regexSymbol.Id.ToString());
                     node.Attributes.Label = regexSymbol.Token;
                     _graph.Nodes.Add(node);
+                    _nodes.Add(node);
                     break;
                 }
 
@@ -39,8 +42,10 @@ namespace META_FA.Visitors.CST
                     node.Attributes.Label = "[Maybe]";
                     _graph.Nodes.Add(node);
 
-                    var edge = new DotEdge(node.Attributes.Id, _graph.Nodes[^2].Attributes.Id);
+                    var edge = new DotEdge(node.Id, _nodes[^1].Id);
                     _graph.Edges.Add(edge);
+                    
+                    _nodes.Add(node);
                     
                     break;
                 }
@@ -53,8 +58,10 @@ namespace META_FA.Visitors.CST
                     node.Attributes.Label = "[One or Infinity]";
                     _graph.Nodes.Add(node);
 
-                    var edge = new DotEdge(node.Attributes.Id, _graph.Nodes[^2].Attributes.Id);
+                    var edge = new DotEdge(node.Id, _nodes[^1].Id);
                     _graph.Edges.Add(edge);
+                    
+                    _nodes.Add(node);
                     
                     break;
                 }
@@ -67,52 +74,56 @@ namespace META_FA.Visitors.CST
                     node.Attributes.Label = "[Zero Or Infinity]";
                     _graph.Nodes.Add(node);
 
-                    var edge = new DotEdge(node.Id, _graph.Nodes[^2].Attributes.Id);
+                    var edge = new DotEdge(node.Id, _nodes[^1].Id);
                     _graph.Edges.Add(edge);
+                    
+                    _nodes.Add(node);
                     
                     break;
                 }
 
                 case RegexString regexString:
                 {
-                    var count = regexString.Children.Count;
-
+                    var nodes = new List<DotNode>();
                     foreach (var child in regexString.Children)
                     {
                         child.Visit(this);
+                        nodes.Add(_nodes[^1]);
                     }
                     
                     var currentNode = new DotNode(regexString.Id.ToString());
                     currentNode.Attributes.Label = "{String}";
                     _graph.Nodes.Add(currentNode);
 
-                    var nodes = _graph.Nodes.GetRange(_graph.Nodes.Count - count, count);
                     foreach (var node in nodes)
                     {
-                        _graph.Edges.Add(new DotEdge(currentNode.Id, node.Attributes.Id));
+                        _graph.Edges.Add(new DotEdge(currentNode.Id, node.Id));
                     }
+                    
+                    _nodes.Add(currentNode);
                     
                     break;
                 }
 
                 case RegexVariant regexVariant:
                 {
-                    var count = regexVariant.Children.Count;
-
+                    var nodes = new List<DotNode>();
                     foreach (var child in regexVariant.Children)
                     {
                         child.Visit(this);
+                        nodes.Add(_nodes[^1]);
                     }
                     
                     var currentNode = new DotNode(regexVariant.Id.ToString());
                     currentNode.Attributes.Label = "{Variant}";
                     _graph.Nodes.Add(currentNode);
 
-                    var nodes = _graph.Nodes.GetRange(_graph.Nodes.Count - count, count);
                     foreach (var node in nodes)
                     {
-                        _graph.Edges.Add(new DotEdge(currentNode.Id, node.Attributes.Id));
+                        _graph.Edges.Add(new DotEdge(currentNode.Id, node.Id));
                     }
+                    
+                    _nodes.Add(currentNode);
                     
                     break;
                 }
