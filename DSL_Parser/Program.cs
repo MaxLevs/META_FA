12584059cf7479f0b96fd6852bd4e381aceeb5e2
Maxim Liven <maxlevs@mail.ru>
2020-12-18 15:03:39 +0300

    Program.cs: Add PrepareData()

--- DSL_Parser/Program.cs
+++ DSL_Parser/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DSL_Parser
 {
@@ -10,13 +11,18 @@ namespace DSL_Parser
             Console.WriteLine("DSL Parser Lib\n");
             var dslParser = DSLGrammar.GetParser();
 
-            using (var dfaExplanation = File.OpenText("../../../Examples/example1.dfa"))
-            {
-                var dfaText = dfaExplanation.ReadToEnd().Replace(Environment.NewLine, " ");
-                var ast = dslParser.Goal.Parse(dfaText);
+            using var dfaExplanation = File.OpenText("../../../Examples/example2.dfa");
+            var dfaText = PrepareData(dfaExplanation.ReadToEnd());
+            var ast = dslParser.Goal.Parse(dfaText);
                 
-                Console.WriteLine(ast?.Dot() ?? "[NULL PARSE RESULT]");
-            }
+            Console.WriteLine(ast?.Dot() ?? "[NULL PARSE RESULT]");
+        }
+
+        private static string PrepareData(string data)
+        {
+            var withoutNewLines = data.Replace(Environment.NewLine, " ");
+            var withoutMultipleSpaces = new Regex("[ ]{2,}", RegexOptions.None).Replace(withoutNewLines, " ");
+            return withoutMultipleSpaces;
         }
     }
 }
