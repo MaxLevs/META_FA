34af6e476fe8e76c4d05fde7e95c9020f97bd70a
Maxim Liven <maxlevs@mail.ru>
2020-12-18 09:29:58 +0300

    Add new project for storing dsl parser

---
+++ DSL_Parser/DSLGrammar.cs
@@ -0,0 +1,33 @@
+﻿using BFParser;
+using BFParser.Parsers;
+
+namespace DSL_Parser
+{
+    public static class DSLGrammar
+    {
+        // Rule names
+        public const string Symbol = "symbol";
+        public const string Quantifier = "quantifier";
+        public const string Element = "element";
+        public const string Variant = "variant";
+        public const string Str = "str";
+
+        public static Grammar GetParser()
+        {
+            var gram = new Grammar(Str)
+            {
+                {Symbol, P.RE(@"[^*?()+|]")},
+                {Quantifier, P.C(Element) + P.T("?") |
+                             P.C(Element) + P.T("*") |
+                             P.C(Element) + P.T("+") |
+                             P.C(Element)},
+                {Element, P.T("(") + P.C(Variant) + P.T(")") | P.C(Symbol)},
+                {Variant, P.C(Str) + P.T("|") + P.C(Variant) | P.C(Str)},
+                {Str, P.OI(P.C(Quantifier))}
+            };
+            gram.InitGrammar();
+        
+            return gram;
+        }
+    }
+}
